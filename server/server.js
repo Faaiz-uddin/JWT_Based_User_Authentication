d3a4f8e4c5e42951a47f0be95a0db3f07bc0838a
Faaiz uddin <faaiz@verge.email>
2023-09-11 14:06:57 +0500

    update server file

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
 const authRoute = require("./routes/RegistrationRoutes");
 const cookieParser = require("cookie-parser"); // Import cookie-parser
+const dotenv = require("dotenv"); // Import dotenv package
+dotenv.config(); // Load environment variables from .env file
+const PORT = process.env.PORT; 
+const MONGO_URL = process.env.MONGO_URL; 
 
-const PORT = 4000;
-const MONGO_URL =
-  "mongodb+srv://crudapp:merncrud@crudapp123.l7ryky0.mongodb.net/login_auth_jwt";
 
 mongoose
   .connect(MONGO_URL, {
